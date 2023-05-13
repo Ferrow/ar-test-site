4000c08d92cfab44720ad119fd5b662157e72cf7
gerhatf <gerhat@ishowroom.cz>
2023-05-13 22:15:48 +0200

    working hotspots

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const btn = document.querySelector("#btn")
+const auto = document.querySelector("#auto")
 
-function hotspotClickOne() {
-    // btn.style.display = btn.style.display === "none" ? "block" : "none";
-    const annotation = document.querySelector(".HotspotAnnotation");
+
+function hotspotClickOne(hotspot) {
+    // auto.cameraOrbit = "80deg, 70deg, 4m"
+    
+    const annotation = hotspot.querySelector(".HotspotAnnotation");
 
     annotation.style.display =
-      annotation.style.display === "block" ? "none" : "block";
+      (annotation.style.display === "block") ? "none" : "block";
   }
-
-// btn.addEventListener("click", () => {
-//     console.log(btn.style.display)
-// })
-
-btn.addEventListener("click", hotspotClickOne)
