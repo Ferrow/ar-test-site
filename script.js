bf3185895b0757f2e07c83293d50785f6d73d576
Ferrow <gerhat.f@gmail.com>
2023-06-19 12:56:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const auto = document.querySelector("#auto")
 
+// Handles loading the events for <model-viewer>'s slotted progress bar
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+
+  if (event.detail.totalProgress === 0) {
+    progressBar.style.display = 'block';
+    updatingBar.style.width = '0%';
+  } else {
+    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+
+    if (event.detail.totalProgress === 1) {
+      setTimeout(() => {
+        progressBar.style.display = 'none';
+      }, 500);
+    }
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
 
 function hotspotClickOne(hotspot) {
     
@@ -29,6 +50,8 @@ function hotspotClickOne(hotspot) {
       // modelV.pause()
       modelV.animationName = "90_19_baked"
     } 
+
+    
     
 
     // modelV.animationName = modelV.animationName === "Wave" ? "Dance" : "Wave"
